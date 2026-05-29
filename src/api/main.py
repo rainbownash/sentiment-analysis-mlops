41604e152acabe737eea94797f889c328b84ea64
rainbownash <anastasie.wagenheim@gmail.com>
2026-05-29 15:01:50 +0200

    Update return from api

--- src/api/main.py
+++ src/api/main.py
@@ -8,10 +8,12 @@ PROJECT_ROOT = Path.cwd()
 
 MODEL_PATH = PROJECT_ROOT / "artifacts/bow/bow_lr.pkl"
 VECTORIZER_PATH = PROJECT_ROOT / "artifacts/bow/bow_vectorizer.pkl"
+LE_PATH = PROJECT_ROOT / "artifacts/le.pkl"
 
 # artefactos
 model = None
 vectorizer = None
+le = None
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -19,6 +21,7 @@ async def lifespan(app: FastAPI):
 
     model = joblib.load(MODEL_PATH)
     vectorizer = joblib.load(VECTORIZER_PATH)
+    le = joblib.load(LE_PATH)
 
     print("Models loaded")
 
@@ -44,8 +47,11 @@ def predict(input_data: TextInput):
     X = vectorizer.transform([input_data.text])
 
     pred = model.predict(X)[0]
+    label = le.inverse_transform([pred])[0]
+    probs = model.predict_proba(X)[0]
 
     return {
         "text": input_data.text,
-        "prediction": int(pred)
+        "prediction": label,
+        "confidence": float(max(probs))
     }
